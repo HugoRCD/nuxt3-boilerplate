98c5a38746ae32bfbcbf57556d80a06cc63fc5aa
HugoRCD <hugo.richard@epitech.eu>
2023-06-17 11:30:46 +0200

    Add app config option

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,7 @@
 export default defineAppConfig({
+  appName: "Nuxt Starter",
+  appDescription: "Nuxt Starter is a starter template for Nuxt3",
+  appVersion: "1.0.0",
   ui: {
     primary: 'emerald',
     gray: 'zinc',
