1b50788de422e8e07e1ff36861bea48f0e41cc4e
HugoRCD <hugo.richard@epitech.eu>
2023-11-12 11:59:03 +0100

    Improve scroll to top

--- components/layout/ScrollToTop.vue
+++ components/layout/ScrollToTop.vue
@@ -21,7 +21,7 @@ function scrollToTop() {
   <div class="fixed z-50 bottom-36 sm:bottom-20 right-3">
     <div
       aria-label="scroll to top button"
-      class="flex items-center justify-center w-10 h-10 p-1 text-muted hover:text-main transition-all duration-200"
+      class="flex items-center rounded-full cursor-pointer justify-center w-10 h-10 p-1 text-muted transition-all duration-200 bg-white dark:bg-zinc-800 shadow-md hover:shadow-lg border border-gray-200 dark:border-zinc-700 hover:border-gray-300 dark:hover:border-zinc-600"
       @click="scrollToTop"
       rounded
       :class="{ 'opacity-100': isScrolling, 'opacity-0': !isScrolling }"
